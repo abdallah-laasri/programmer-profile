8b915c36a9283dd61d4369296a25b261ad19dd4e
abdallahlaasri <abdallahlaasri@gmail.com>
2019-12-22 13:11:40 +0100

    Updates

---
+++ precache-manifest.c9ab892e5e7a593c3f10e23e1849311a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57d2af1083eab5ab850eae9ab7c6a24",
+    "url": "/programmer-profile/index.html"
+  },
+  {
+    "revision": "a5fe1db0da7feb8d5795",
+    "url": "/programmer-profile/static/css/2.dd315c68.chunk.css"
+  },
+  {
+    "revision": "2a768ef0843cffe2e13a",
+    "url": "/programmer-profile/static/css/main.651dc82d.chunk.css"
+  },
+  {
+    "revision": "a5fe1db0da7feb8d5795",
+    "url": "/programmer-profile/static/js/2.4b47a908.chunk.js"
+  },
+  {
+    "revision": "2a768ef0843cffe2e13a",
+    "url": "/programmer-profile/static/js/main.bfee8efc.chunk.js"
+  },
+  {
+    "revision": "2d947ee858314450e2e0",
+    "url": "/programmer-profile/static/js/runtime~main.b5d95856.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/programmer-profile/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/programmer-profile/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/programmer-profile/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/programmer-profile/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/programmer-profile/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a63b4c5aee27256c88934dec50f09e17",
+    "url": "/programmer-profile/static/media/home-avatar.a63b4c5a.PNG"
+  },
+  {
+    "revision": "8d7c63889fc1bc2d3b9e959f3b4a19af",
+    "url": "/programmer-profile/static/media/home-bg.8d7c6388.jpg"
+  }
+]);
